76dc57bc652631fcbfe9ffcd476119c14c7bf3c7
Chris McGivney <cmmcgivney84@gmail.com>
2020-05-18 21:41:05 -0600

    sitemmap configs

---
+++ src/sitemap/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+ 
+export default (
+    <Switch>
+	   <Route exact path='/' />
+     <Route exact path='/Contact' />
+	   <Route exact path='/Products' />
+	   <Route exact path='/FAQ' />
+    </Switch>
+);
