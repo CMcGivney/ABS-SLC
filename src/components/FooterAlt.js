d86bcf74b7bbbc61bd95f42e37696274d21536df
Chris McGivney <cmmcgivney84@gmail.com>
2020-06-01 01:12:41 -0600

    link refactoring

--- src/components/FooterAlt.js
+++ src/components/FooterAlt.js
@@ -19,11 +19,11 @@ class Footer extends React.Component {
            </div>
           <div className='bizAddress'>
             <Header.Content>
-            <a className="email" href="https://www.google.com/maps/place/67+W+1700+S,+Salt+Lake+City,+UT+84115/@40.7335027,-111.8955317,17z/data=!3m1!4b1!4m5!3m4!1s0x8752f52d54f48d85:0xad519ab8427c7144!8m2!3d40.7335027!4d-111.893343">67 West 1700 South SLC, UT 84104</a>
+            <a className="email"  target="_blank" rel="noopener noreferrer"href="https://www.google.com/maps/place/67+W+1700+S,+Salt+Lake+City,+UT+84115/@40.7335027,-111.8955317,17z/data=!3m1!4b1!4m5!3m4!1s0x8752f52d54f48d85:0xad519ab8427c7144!8m2!3d40.7335027!4d-111.893343">67 West 1700 South SLC, UT 84104</a>
             </Header.Content>
            </div>
           <div className='emailBox'>
-             <h4>email:  <a className="email" href="mailto:Stan@absseafood.com?subject=Mailed from Website">Stan@ABSSeafood.com</a></h4>  
+             <h4>email:  <a className="email"  target="_blank" rel="noopener noreferrer" href="mailto:Stan@absseafood.com?subject=Mailed from Website">Stan@ABSSeafood.com</a></h4>  
          </div>
           <div >
             <Header.Content>
--- src/components/SocialFollows.js
+++ src/components/SocialFollows.js
@@ -7,16 +7,23 @@ export default function SocialFollow() {
   return (
     <div className="social-container">
      
-      <a href="https://twitter.com/ABSSeafoodSF" className="twitter social">
+      <a 
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://twitter.com/ABS_Seafood_SLC" className="twitter social">
         <FontAwesomeIcon icon={faTwitter} size="2x" />
       </a>
       <a
+        target="_blank"
+        rel="noopener noreferrer"
         href="https://www.instagram.com/absseafood/?hl=en"
         className="instagram social"
       >
         <FontAwesomeIcon icon={faInstagram} size="2x" />
       </a>
       <a
+        target="_blank"
+        rel="noopener noreferrer"
         href="https://www.facebook.com/ABSseafood"
         className="facebook social"
       >
