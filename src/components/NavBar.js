3f464f47a9317d3e36dddba9543efcb8ff58a3fe
Chris McGivney <cmmcgivney84@gmail.com>
2020-08-24 16:24:34 -0600

    typo fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link, withRouter} from 'react-router-dom'
 import {Menu, Image} from 'semantic-ui-react'
 import DropDownMenu from './DropDownMenu.js'
-import Logo from '../images/ABS-Black-NoBackground.png'
+import Logo from '../images/ABS-Black-Nobackground.png'
 
 const navStyles = {
   padding: '1rem',
