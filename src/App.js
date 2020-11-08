4d95263156b9f15476fca2d9b59013c6dce6ffa8
Chris McGivney <cmmcgivney84@gmail.com>
2020-11-08 08:53:03 -0700

    work: troubleshooting GA

--- src/App.js
+++ src/App.js
@@ -1,58 +1,55 @@
-import React from 'react';
-import {Route, Switch, useHistory } from "react-router-dom";
-import Home from './components/Home'
-import Contact from './components/Contact'
-import Inventory from './components/Inventory'
-import FAQ from './components/FAQ'
-import NavBar from './components/NavBar'
-import Footer from './components/FooterAlt'
-import NoMatch from './components/NoMatch'
-import ScrollToTop from './components/scrollToTop'
-import './App.css'
+import React from "react";
+import { Route, Switch, useHistory } from "react-router-dom";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Inventory from "./components/Inventory";
+import FAQ from "./components/FAQ";
+import NavBar from "./components/NavBar";
+import Footer from "./components/FooterAlt";
+import NoMatch from "./components/NoMatch";
+import ScrollToTop from "./components/scrollToTop";
+import "./App.css";
 // import TagManager from 'react-gtm-module'
-import ReactGA from 'react-ga'
-
+import ReactGA from "react-ga";
 
 function App() {
-  ReactGA.initialize('G-LSC0KJ6YGL');
+  ReactGA.initialize("G-LSC0KJ6YGL");
 
   // const tagManagerArgs = {
   //   gtmId: 'GTM-MPJMMFW'
   // }
   // TagManager.initialize(tagManagerArgs)
-  
+
   window.dataLayer.push({
-    event: 'event',
+    event: "event",
     eventProps: {
       category: "pageview",
       action: "getQuote",
       label: "button push",
-      value: 1
-  }
+      value: 1,
+    },
   });
   const history = useHistory();
 
   history.listen((location) => {
     ReactGA.set({ page: location.pathname });
-    ReactGA.pageview(location.pathname)
-   }
-  );
-  console.log(history.location.pathname)
+    ReactGA.pageview(location.pathname);
+  });
+  console.log(history.location.pathname);
   return (
-  <>
-   
-    <NavBar/>
-     <ScrollToTop/>
-     <Switch>
-     <Route exact path='/' component={Home} />
-     <Route exact path='/Contact' component={Contact} />
-     <Route exact path='/Products' component={Inventory} />
-     <Route exact path='/FAQ' component={FAQ} />
-     <Route component={NoMatch} />
-     </Switch>
-   
-    <Footer/>
-  </>
+    <>
+      <NavBar />
+      <ScrollToTop />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Contact" component={Contact} />
+        <Route exact path="/Products" component={Inventory} />
+        <Route exact path="/FAQ" component={FAQ} />
+        <Route component={NoMatch} />
+      </Switch>
+
+      <Footer />
+    </>
   );
 }
 
